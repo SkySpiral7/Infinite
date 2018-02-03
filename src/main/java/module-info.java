73fcb1851eb2716ebdcbf9c525f186d688894600
Ryan Worcester <rworcest@g.emporia.edu>
2018-02-03 11:01:31 -0600

    Converted to java module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.github.skySpiral7.java.infinite {
+   requires Java;  //Automatic Module: com.github.SkySpiral7:Java
+   requires com.github.skySpiral7.java.staticSerialization;  //com.github.SkySpiral7:Java.StaticSerialization
+   exports com.github.skySpiral7.java.infinite.dataStructures;
+   exports com.github.skySpiral7.java.infinite.numbers;
+}
